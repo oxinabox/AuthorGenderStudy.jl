541086c1c9d127c7e56ddb80e70e5bf2a796f044
Lyndon White <oxinabox@ucc.asn.au>
2018-06-06 00:07:55 +0800

    =add preprocessing

--- src/AuthorGenderStudy.jl
+++ src/AuthorGenderStudy.jl
@@ -6,10 +6,16 @@ using JSON
 using Glob
 using DataDeps
 using NamedTuples
+using InternedStrings
+using NameToGender
+using WordTokenizers
 
 function __init__()
     include("init_datadeps.jl")
 end
+
+
+export load_data, preprocess_data
 
 include("load_data.jl")
 
--- src/load_data.jl
+++ src/load_data.jl
@@ -22,3 +22,35 @@ function load_data(datadir = datadep"Open Research Corpus")
     end
 
 end
+
+
+
+function preprocess_data(data)
+    Channel(ctype=Any, csize=2^12) do ch
+        for datum in data
+            (length(datum.authors)==0 || length(datum.paper_abstract)==0) && @goto next_datum
+
+            author_genders = GenderUsage[]
+            for author in datum.authors
+                firstname = first(split(author, " "; limit=2, keep=false))
+                length(firstname) <= 2 && @goto next_datum
+                try
+                    push!(author_genders, classify_gender(firstname))
+                catch err
+                    err isa KeyError || rethrow()
+                    @goto next_datum
+                end
+            end
+                
+            abstract_sents = tokenize.(split_sentences(datum.paper_abstract))
+
+            paper_abstract = intern.(foldl(vcat, abstract_sents))
+            put!(ch, @NT(authors=datum.authors,
+                         author_genders=author_genders,
+                         paper_abstract=paper_abstract))
+            @label next_datum
+        end
+    end
+end
+
+
