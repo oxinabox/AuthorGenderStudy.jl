22599334b09e814d1a54523f9d97ad1cc25b1e57
Lyndon White <oxinabox@ucc.asn.au>
2018-06-20 16:51:28 +0800

    =distribute loading

--- src/load_data.jl
+++ src/load_data.jl
@@ -1,31 +1,20 @@
-const BUFFER_SIZE = 2^12
-
-
-function extract_data(line)
-    data = JSON.parse(line)
-    @NT(
-        authors = [author["name"] for author in data["authors"]],
-        paper_abstract = data["paperAbstract"]
-    )
-end
+const BUFFER_SIZE = 2^6
 
 
 function load_data(datadir = datadep"Open Research Corpus")
-    Channel(ctype=Any, csize=BUFFER_SIZE) do ch
-        @sync for fn in glob("*.gz", datadir)
-            @async for line in eachline(ZlibInflateInputStream(open(fn)))
-                put!(ch, extract_data(line))
-            end
-        end
+    function extract_data(line)
+        data = JSON.parse(line)
+        @NT(
+            authors = [author["name"] for author in data["authors"]],
+            paper_abstract = data["paperAbstract"]
+        )
     end
 
-end
+    function process_file(ch, fn)
+        for line in eachline(ZlibInflateInputStream(open(fn)))
+            datum = extract_data(line)
 
-
-
-function preprocess_data(data = load_data())
-    Channel(ctype=Any, csize=BUFFER_SIZE) do ch
-        for datum in data
+            #println(datum.authors)
             (length(datum.authors)==0 || length(datum.paper_abstract)==0) && continue
 
             firstnames = first.(split.(datum.authors, " "; limit=2, keep=false))
@@ -40,10 +29,41 @@ function preprocess_data(data = load_data())
 
             abstract_sents = tokenize.(split_sentences(datum.paper_abstract))
 
-            paper_abstract = intern.(reduce(vcat, abstract_sents))
+            paper_abstract = String.(reduce(vcat, abstract_sents))
             put!(ch, @NT(author_gender=first_author_gender,
                          paper_abstract=paper_abstract))
+
         end
+    end
+
+    ##################
+    # Actual function 
+
+    local_ch = Channel{Any}(BUFFER_SIZE)
+    remote_ch = RemoteChannel(()->local_ch)
+
+    c_pool = CachingPool(workers())
+    filenames = glob("*.gz", datadir)
+    file_dones = remotecall.(process_file, c_pool, remote_ch, filenames)
+    
+
+    # schedule the following to occur at some point on the local worker
+    @schedule begin # Close off the channel once all files are done
+        for file_done in file_dones
+            wait(file_done)
+        end # wait for all of them
+        close(local_ch)
+        clear!(c_pool)
+    end
+
+    return local_ch
+end
+
+
+function preprocess_data(data = load_data())
+    Channel(ctype=Any, csize=BUFFER_SIZE) do ch
+        for datum in data
+       end
     end
 end
 
